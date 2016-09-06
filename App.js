233f26798e2a1f5b68acd771a3debb3cd084e2ec
Sergey Rubets <sramator@gmail.com>
2016-09-06 13:58:30 +0300

    First component

--- App.js
+++ App.js
@@ -2,10 +2,14 @@
 
 import React from 'React';
 
+// Have state
 class App extends React.Component {
   render() {
-    return <div>Hello</div>
+    return <div>HI</div>
   }
 }
+
+// doesn't have state
+// const App = () => <h1>Hello</h1>
 
 export default App
