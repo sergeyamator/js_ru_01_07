7070f7818e184803698cac2e80f9892441fbac35
Sergey Rubets <sramator@gmail.com>
2016-09-06 15:30:27 +0300

    Change state with input event

--- App.js
+++ App.js
@@ -2,11 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render() {
-    let txt = this.props.txt;
-    let cat = this.props.cat;
+  constructor() {
+    super();
 
-    return <h1>{txt}  {cat}</h1>
+    this.state = {
+      txt: 'this is the state txt',
+      cat: 0
+    };
+  }
+
+  update(e) {
+    this.setState({
+      txt: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text"
+               onChange={this.update.bind(this)} />
+
+        <h1>{this.state.txt}</h1>
+      </div>
+    )
   }
 }
 
